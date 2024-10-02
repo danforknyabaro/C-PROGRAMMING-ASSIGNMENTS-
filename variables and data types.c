a4e393dd918f071948542f0c8b19560e9b4557b7
danforknyabaro <nyabarodanforkrioba@gmail.com>
2024-10-02 17:25:32 +0300

    Add files via upload

---
+++ variables and data types.c
@@ -0,0 +1,17 @@
+//variables and data types
+//preposessor directives-scanf(), printf()
+#include<stdio.h>
+int main(){
+    //declaration and initialization
+    char a='s';//
+    char name []=("Danfork");
+    int age=20;
+    float salary=20000;
+    double budget=1000;
+printf("the character is %c \n", a);
+printf("the name is %s \n", name);
+printf("the age is %d \n", age);
+printf("the salary is %f \n", salary);
+printf("the budget is %Lf \n", budget);
+return 0;
+}
